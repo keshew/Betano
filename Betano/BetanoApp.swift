95aadc5937e4e1df3e83251bc295a0706f622031
keshew <artyom.korotckov2017@yandex.ru>
2025-04-04 18:49:31 +0500

    Initial Commit

---
+++ Betano/BetanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetanoApp.swift
+//  Betano
+//
+//  Created by Артём Коротков on 04.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
